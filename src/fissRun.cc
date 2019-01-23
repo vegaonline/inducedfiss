63dfe7afe9aeb55448eb3209bc75996f6a3a42d3
Abhijit Bhattacharyya <abhihere@gmail.com>
2019-01-23 19:49:12 +0530

    FissRun edited. Needs correction. No time.

--- src/fissRun.cc
+++ src/fissRun.cc
@@ -186,28 +186,140 @@ void fissRun::EndOfRun() {
  // compute mean Energy deposited and rms in target
   //
   G4int TotNumberOFevents = numberOfEvent;
-  fEdepTarget1 /= TotNumberOFevents; fEdepTarget2 /= TotNumberOFevents;
-  G4double rmsEdep = fEdepTarget2 - fEdepTarget*fEdepTarget;
+  fEdepTarget1 /= TotNumberOFevents;
+  fEdepTarget2 /= TotNumberOFevents;
+  G4double rmsEdep = fEdepTarget2 - fEdepTarget1 * fEdepTarget1;
   if (rmsEdep>0.) rmsEdep = std::sqrt(rmsEdep);
-  else            rmsEdep = 0.;
+  else                    rmsEdep = 0.;
 
   G4cout << "\n Mean energy deposit in target,   in time window = "
-         << G4BestUnit(fEdepTarget,"Energy") << ";  rms = "
+         << G4BestUnit(fEdepTarget1,"Energy") << ";  rms = "
          << G4BestUnit(rmsEdep,    "Energy")
          << G4endl;
 
   // compute mean Energy deposited and rms in detector
   //
-  fEdepDetect /= TotNbofEvents; fEdepDetect2 /= TotNbofEvents;
-  rmsEdep = fEdepDetect2 - fEdepDetect*fEdepDetect;
-  if (rmsEdep>0.) rmsEdep = std::sqrt(rmsEdep);
-  else            rmsEdep = 0.;
+  fEdepDetect1 /= TotNumberOFevents;
+  fEdepDetect2 /= TotNumberOFevents;
+  rmsEdep = fEdepDetect2 - fEdepDetect1 * fEdepDetect1;
+  if (rmsEdep > 0.0) rmsEdep = std::sqrt(rmsEdep);
+  else                    rmsEdep = 0.;
 
   G4cout << " Mean energy deposit in detector, in time window = "
-         << G4BestUnit(fEdepDetect,"Energy") << ";  rms = "
+         << G4BestUnit(fEdepDetect1,"Energy") << ";  rms = "
          << G4BestUnit(rmsEdep,    "Energy")
          << G4endl;
 
+  // frequency of processes in target
+  //
+  G4cout << "\n Process calls frequency in target :" << G4endl;
+  G4int index = 0;
+  std::map<G4String,G4int>::iterator it1;
+  for (it1 = fProcCounter1.begin(); it1 != fProcCounter1.end(); it1++) {
+    G4String procName = it1->first;
+    G4int    count    = it1->second;
+    G4String space = " "; if (++index%3 == 0) space = "\n";
+    G4cout << " " << std::setw(20) << procName << "="<< std::setw(7) << count << space;
+  }
+  G4cout << G4endl;
 
+  // frequency of processes in detector
+  //
+  G4cout << "\n Process calls frequency in detector:" << G4endl;
+  index = 0;
+  std::map<G4String,G4int>::iterator it2;
+  for (it2 = fProcCounter2.begin(); it2 != fProcCounter2.end(); it2++) {
+    G4String procName = it2->first;
+    G4int    count    = it2->second;
+    G4String space = " "; if (++index%3 == 0) space = "\n";
+    G4cout << " " << std::setw(20) << procName << "="<< std::setw(7) << count << space;
+  }
+  G4cout << G4endl;
 
+  // particles count in target
+  //
+  G4cout << "\n List of generated particles in target:" << G4endl;
+
+  std::map<G4String,ParticleData>::iterator itc;
+  for (itc = fParticleDataMap1.begin(); itc != fParticleDataMap1.end(); itc++) {
+    G4String name = itc->first;
+    ParticleData data = itc->second;
+    G4int count = data.fCount;
+    G4double eMean = data.fEmean/count;
+    G4double eMin = data.fEmin;
+    G4double eMax = data.fEmax;
+
+    G4cout << "  " << std::setw(13) << name << ": " << std::setw(7) << count
+             << "  Emean = " << std::setw(wid) << G4BestUnit(eMean, "Energy")
+             << "\t( "  << G4BestUnit(eMin, "Energy")
+             << " --> " << G4BestUnit(eMax, "Energy")
+             << ")" << G4endl;
+  }
+
+  // particles count in detector
+  //
+  G4cout << "\n List of generated particles in detector:" << G4endl;
+
+  std::map<G4String,ParticleData>::iterator itn;
+  for (itn = fParticleDataMap2.begin(); itn != fParticleDataMap2.end(); itn++) {
+    G4String name = itn->first;
+    ParticleData data = itn->second;
+    G4int count = data.fCount;
+    G4double eMean = data.fEmean/count;
+    G4double eMin = data.fEmin;
+    G4double eMax = data.fEmax;
+
+    G4cout << "  " << std::setw(13) << name << ": " << std::setw(7) << count
+                << "  Emean = " << std::setw(wid) << G4BestUnit(eMean, "Energy")
+                << "\t( "  << G4BestUnit(eMin, "Energy")
+                << " --> " << G4BestUnit(eMax, "Energy") << ")" << G4endl;
+  }
+  G4cout << G4endl;
+
+  // activities in VR mode
+  //
+  WriteActivity(numberOfEvent);
+
+  //remove all contents in fProcCounter, fCount
+  fProcCounter1.clear();
+  fProcCounter2.clear();
+  fParticleDataMap1.clear();
+  fParticleDataMap2.clear();
+
+  //restore default format
+  G4cout.precision(dfprec);
 }
+
+void fissRun::WriteActivity(G4int nevent) {
+  G4ProcessTable *pTable = G4ProcessTable::GetProcessTable();
+  G4RadioactiveDecay * rDecay = (G4RadioactiveDecay *)
+  pTable->FindProcess("RadioactiveDecay", "GenericIon");
+
+  // output the induced radioactivities (in VR mode only)
+  //
+  if ((rDecay == 0) || (rDecay->IsAnalogueMonteCarlo())) return;
+  G4String fileName = G4AnalysisManager::Instance()->GetFileName() + ".activity";
+  std::ofstream outfile (fileName, std::ios::out );
+
+  std::vector<G4RadioactivityTable*> theTables =  rDecay->GetTheRadioactivityTables();
+
+  for (size_t i = 0 ; i < theTables.size(); i++) {
+    G4double rate, error;
+   outfile << "Radioactivities in decay window no. " << i << G4endl;
+   outfile << "Z \tA \tE \tActivity (decays/window) \tError (decays/window) "
+   << G4endl;
+
+  map<G4ThreeVector,G4TwoVector> *aMap = theTables[i]->GetTheMap();
+  map<G4ThreeVector,G4TwoVector>::iterator iter;
+  for (iter=aMap->begin(); iter != aMap->end(); iter++) {
+  rate = iter->second.x()/nevent;
+  error = std::sqrt(iter->second.y())/nevent;
+  if (rate < 0.) rate = 0.;                // statically it can be < 0.
+  outfile << iter->first.x() <<"\t"<< iter->first.y() <<"\t"
+  << iter->first.z() << "\t" << rate <<"\t" << error << G4endl;
+  }
+  outfile << G4endl;
+  }
+  outfile.close();
+  }
+   
