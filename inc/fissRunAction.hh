c824454bd8702e2c5bb6c151dfbb4a75f1fa8a05
Abhijit Bhattacharyya <abhihere@gmail.com>
2019-01-23 12:28:05 +0530

    RunAction added. Check whether U238 added in Target.

---
+++ inc/fissRunAction.hh
@@ -0,0 +1,43 @@
+//*************************************************************
+//*       Code for Fast neutron induced fission fragments     *
+//*                                                           *
+//*      Abhijit Bhattacharyya  :: NPD :: BARC                *
+//*     Jan 22, 2019                                          *
+//*    \file fissRunAction.hh                                *
+//*************************************************************
+
+//  $Id: fissRunAction.hh Jan 23, 2019    11:02:15Z   vega $
+
+#ifndef fissRunAction_h
+#define fissRunAction_h 1
+
+#include "G4UserRunAction.hh"
+#include "fissDetectorConstruction.hh"
+#include "fissPrimaryGeneratorAction.hh"
+//#include "HistoManager.hh"
+
+#include "G4Run.hh"
+#include "G4RunManager.hh"
+#include "G4UnitsTable.hh"
+#include "G4SystemOfUnits.hh"
+
+#include "Randomize.hh"
+#include <iomanip>
+#include "globals.hh"
+
+class fissRunAction : public G4UserRunAction {
+public:
+  fissRunAction(fissDetectorConstruction*, fissPrimaryGeneratorAction*);
+  ~fissRunAction();
+
+public:
+  virtual G4Run* GenerateRun();
+  virtual void BeginOfRunAction(const G4Run*);
+  virtual void EndOfRunAction(const G4Run*);
+
+private:
+  fissDetectorConstruction* fDetector;
+  fissPrimaryGeneratorAction* fPrimary;
+};
+
+#endif
---
+++ src/fissRunAction.cc
@@ -0,0 +1,38 @@
+//*************************************************************
+//*       Code for Fast neutron induced fission fragments     *
+//*                                                           *
+//*      Abhijit Bhattacharyya  :: NPD :: BARC                *
+//*     Jan 22, 2019                                          *
+//*    \file fissRunAction.cc                                *
+//*************************************************************
+
+//  $Id: fissRunAction.cc Jan 23, 2019    11:10:45Z   vega $
+
+#include "fissRunAction.hh"
+
+fissRunAction::fissRunAction(fissDetectorConstruction* det, fissPrimaryGeneratorAction* fprim)
+  : G4UserRunAction(), fDetector(det), fPrimary(fprim){
+
+}
+
+fissRunAction::~fissRunAction() {
+
+}
+
+G4Run* fissRunAction::GenerateRun(){
+
+}
+
+void fissRunAction::BeginOfRunAction(const G4Run*) {
+  G4RunManager::GetRunManager()->SetRandomNumberStore(false);
+  if (isMaster) G4Random->showEngineStatus();
+
+  if (fPrimary){
+    G4ParticleDefinition* particle = fPrimary->GetParticleGun()->GetParticleDefinition();
+    G4double energy = fPrimary->GetParticleGun()->GetEnergy();
+  }
+}
+
+void fissRunAction::EndOfRunAction(const G4Run*){
+  if (isMaster) G4Random::showEngineStatus();
+}
